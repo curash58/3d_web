7127ace1eec7384c9634e4d531da5f5f8fe26b31
Arsen_Valeev_SectionC <156844701+ArsenValeevSectionC@users.noreply.github.com>
2025-01-05 11:05:02 -0500

    finish hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,34 +5,15 @@ import CanvasLoader from "../components/CanvasLoader";
 import { PerspectiveCamera } from "@react-three/drei";
 import { Leva, useControls } from "leva";
 import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from "../constants/index.js";
+import Target from "../components/Target.jsx";
+import ReactLogo from "../components/ReactLogo.jsx";
+import Cube from "../components/Cube.jsx";
+import Rings from "../components/Ring.jsx";
+import HeroCamera from "../components/HeroCamera.jsx";
+import Button from "../components/Button.jsx";
 
 const Hero = () => {
-  const x = useControls("HackerRoom", {
-    positionX: {
-      value: 2.5,
-      min: -10,
-      max: 10,
-    },
-    positionY: {
-      value: 2.5,
-      min: -10,
-      max: 10,
-    },
-    positionZ: {
-      value: 2.5,
-      min: -10,
-      max: 10,
-    },
-    rotationX: { value: 2.5, min: -10, max: 10 },
-    rotationY: { value: 2.5, min: -10, max: 10 },
-    rotationZ: { value: 2.5, min: -10, max: 10 },
-    scale: {
-      value: 1,
-      min: 0.01,
-      max: 0.1,
-    },
-  });
-
   const isSmall = useMediaQuery({ maxWidth: 440 });
   const isMobile = useMediaQuery({ maxWidth: 768 });
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
@@ -54,21 +35,36 @@ const Hero = () => {
       </div>
 
       <div className="w-full h-full absolute inset-0">
-        <Leva />
+        {/* <Leva /> */}
         <Canvas className="w-full h-full">
           <Suspense fallback={<CanvasLoader />}>
-            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
-            <HackerRoom
-              //   scale={0.07}
-              position={sizes.position}
-              rotation={[0, -Math.PI, 0]}
-              scale={sizes.deskScale}
-            />
+            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+            <HeroCamera isMobile = {isMobile}>
+              <HackerRoom
+                //   scale={0.07}
+                position={sizes.deskPosition}
+                rotation={[0, -Math.PI, 0]}
+                scale={sizes.deskScale}
+              />
+            </HeroCamera>
+
+            <group>
+              <Target position={sizes.targetPosition} />
+              <ReactLogo position={sizes.reactLogoPosition} />
+              <Cube position={sizes.cubePosition} />
+              <Rings position={sizes.ringPosition} />
+            </group>
+
             <ambientLight intensity={1} />
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
           </Suspense>
         </Canvas>
       </div>
+        <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
+            <a href="#contact" className="w-fit">
+                <Button name = "Let's work together" isbeam containerClass="sm:w-fit w-full sm:min-w-96" />
+            </a>
+        </div>
     </section>
   );
 };
